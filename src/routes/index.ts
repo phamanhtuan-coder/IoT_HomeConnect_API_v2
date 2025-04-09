c713c760b3b780a8da40d1d6311a63f38419b9ec
phamanhtuan-coder <0306221391@caothang.edu.vn>
2025-04-09 20:39:23 +0700

    Remove unused user and employee routes from index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes';
-import userRoutes from './user.routes';
-import employeeRoutes from './employee.routes';
+
 import { NotificationController } from '../controllers/notification.controller';
 import userDeviceRoutes from "./user-device.routes";
 import syncTrackingRoutes from "./sync-tracking.routes";
@@ -10,8 +9,6 @@ const router = Router();
 const notificationController = new NotificationController();
 
 router.use('/auth', authRoutes);
-router.use('/users', userRoutes);
-router.use('/employees', employeeRoutes);
 router.post('/notify/otp', notificationController.sendOtp); // New test endpoint
 router.use('/user-devices', userDeviceRoutes);
 router.use('/sync-tracking', syncTrackingRoutes);
