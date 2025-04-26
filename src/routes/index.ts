a1c996693c9e6ee8b638d05ed2b882b8dfd90e39
phamanhtuan-coder <0306221391@caothang.edu.vn>
2025-04-26 23:01:52 +0700

    Add routes for house and space management

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,8 @@ import { NotificationController } from '../controllers/notification.controller';
 import userDeviceRoutes from "./user-device.routes";
 import syncTrackingRoutes from "./sync-tracking.routes";
 import groupRoutes from "./group.routes";
+import houseRoutes from "./house.routes";
+import spaceRoutes from "./space.routes";
 
 const router = Router();
 const notificationController = new NotificationController();
@@ -14,5 +16,7 @@ router.post('/notify/otp', notificationController.sendOtp); // New test endpoint
 router.use('/user-devices', userDeviceRoutes);
 router.use('/sync-tracking', syncTrackingRoutes);
 router.use('/groups', groupRoutes);
+router.use('/houses', houseRoutes); // Add this line
+router.use('/spaces', spaceRoutes); // Add this line
 
 export default router;
