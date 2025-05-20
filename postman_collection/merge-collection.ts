0685aaaeaaa739ec24d017d1bcc6b3a109509220
phamanhtuan-coder <0306221391@caothang.edu.vn>
2025-05-20 22:12:45 +0700

    feat(postman): thêm collection hourly value cho postman
    - Thêm collection hourly value vào postman để test api hourly value
    - Cập nhật file merge collection để merge collection hourly value vào full collection
    
    feat(postman): add hourly value collection to postman
    - Add hourly value collection to postman to test hourly value api
    - Update merge collection file to merge hourly value collection into full collection

--- postman_collection/merge-collection.ts
+++ postman_collection/merge-collection.ts
@@ -17,7 +17,7 @@ interface PostmanCollection {
 
 const basePath = path.join(__dirname);
 const base: PostmanCollection = JSON.parse(fs.readFileSync(path.join(basePath, 'base.json'), 'utf8'));
-const modules = ['auth', 'sync-tracking', 'user-devices', 'notification','device','group','house','space','sharedPermission','shareRequest','alert-type','alert','firmware','firmware-update-history','ownershipHistory','ticket-type','ticket'];
+const modules = ['auth', 'sync-tracking', 'user-devices', 'notification','device','group','house','space','sharedPermission','shareRequest','alert-type','alert','firmware','firmware-update-history','ownershipHistory','ticket-type','ticket','hourly-value'];
 
 const items: any[] = [];
 for (const module of modules) {
